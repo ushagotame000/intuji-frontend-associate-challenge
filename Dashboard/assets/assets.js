e275cdb6e60a6ad3763fadcfd4f1bfe006b9f501
Usha_Gotame <ushagotame000@gmail.com>
2025-03-30 14:40:44 +0545

    feat: sidebar design implementation

---
+++ Dashboard/assets/assets.js
@@ -0,0 +1,14 @@
+const assets = {
+logo: "./icons/gallery.png",
+calender: "./icons/Calendar.png",
+dashboard: "./icons/dashboard.png",
+wallet: "./icons/empty-wallet.png",
+login: "./icons/login.png",
+message: "./icons/message-text.png",
+notification: "./icons/notification-bing.png",
+sms: "./icons/sms.png",
+user: "./icons/user.png",
+profile: "./icons/profile-2user.png",
+save: "./icons/save-2.png",
+info: "./icons/info-circle.png"
+}
